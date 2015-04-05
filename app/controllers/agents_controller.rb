3576fee4efe680a143fe155740513874e0014eb2
Ryan Erickson <rserickson92@gmail.com>
2015-04-05 01:37:06 -0700

    implement POST to /agent

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,7 +1,14 @@
 class AgentsController < ApplicationController
+  respond_to :json
+
   def create
+  	Agent.store(agent_params)
   end
 
   def index
+  end
+
+  def agent_params
+  	params.require(:agent).permit(:worth, :name, :agentType)
   end
 end
--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,2 +1,6 @@
 class Agent < ActiveRecord::Base
+	def self.store(agent)
+		agent["worth"] = (agent["worth"]*100).to_i
+		Agent.create(agent)
+	end
 end
