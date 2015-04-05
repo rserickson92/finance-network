b8f37b813a1b8ec65aca9c12ee6f8de2e2e74805
Ryan Erickson <rserickson92@gmail.com>
2015-04-05 02:18:20 -0700

    implement POST to /agents

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -5,10 +5,22 @@ class AgentsController < ApplicationController
   	Agent.store(agent_params)
   end
 
+  def create_many
+  	agents_params[:agents].each do |agent|
+  	  Agent.store(agent)
+  	end
+  end
+
   def index
   end
 
+  private
+
   def agent_params
   	params.require(:agent).permit(:worth, :name, :agentType)
+  end
+
+  def agents_params
+  	params.permit(agents: [:worth, :name, :agentType])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'agents#index'
-  post 'agent', to: "agents#create"
+  post 'agent', to: 'agents#create'
+  post 'agents', to: 'agents#create_many'
   resources :events, only: [:create]
-  resources :agents, only: [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
