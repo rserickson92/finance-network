89221f6ccb5e726d82ae085cb267faeebe3d1d62
Ryan Erickson <rserickson92@gmail.com>
2015-04-05 13:49:36 -0700

    fix route, should be event instead of events

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'agents#index'
   post 'agent', to: 'agents#create'
   post 'agents', to: 'agents#create_many'
-  resources :events, only: [:create]
+  post 'event', to: 'events#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
