625f59af45c6c42ee8a287cfaf0b05aa88e4fab4
Ryan Erickson <rserickson92@gmail.com>
2015-04-06 03:03:29 -0700

    implement event money exchange

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,14 @@
 class Event < ActiveRecord::Base
 	def self.store(event)
 		event["dateTime"] = event["dateTime"].to_datetime
+		amt = (event["amount"]*100).to_i
+		event["amount"] = amt
 		Event.create(event)
+
+		fromAgent = Agent.find_by_name(event["fromAgent"])
+		toAgent = Agent.find_by_name(event["toAgent"])
+		fromAgent.update(worth: fromAgent.worth - amt)
+		toAgent.update(worth: toAgent.worth + amt)
 	end
 
 	def self.load_all
